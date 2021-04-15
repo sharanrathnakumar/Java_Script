10643f2855062df3ab01fce75ea7fa9b83ac9ba6
Sharan Rathnakumar <53375728+sharanrathnakumar@users.noreply.github.com>
2021-04-15 15:36:03 +0530

    Add files via upload

---
+++ todaysEntries.js
@@ -0,0 +1,28 @@
+/* The function should return the names of those entries whose date is the current date. The names should be separated by a comma.
+
+For example, here's an array of three entries and assume the date now is 2021-01-21.
+
+[{ name: "Johny" , date: "2021-01-21T02:53:42+05:30" }, { name: "Sugar" , date: "2021-01-22T02:53:42+05:30" }, { name: "Sun" , date: "2021-01-21T03:53:42+05:30" }]
+
+The function should return: ``` Johny,Sun ```. Please note that the names are separated by a comma without space.
+*/
+
+function todaysEntries(entries) {
+  // Complete the function
+  td = new Date();
+  todaysDate = td.getFullYear() + "-" + td.getMonth() + 1 + "-" + td.getDate();
+  let nameArray = [];
+
+  //Get date array
+  for (let i = 0; i < entries.length; i++) {
+    let newArray = entries[i].date;
+    let ad = new Date(newArray);
+    dateArray = ad.getFullYear() + "-" + ad.getMonth() + 1 + "-" + ad.getDate();
+    if (dateArray === todaysDate) {
+      nameArray.push(entries[i].name);
+    }
+  }
+  return nameArray.toString();
+}
+
+module.exports = todaysEntries;
